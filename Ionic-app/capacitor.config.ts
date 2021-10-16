d66db0552801f47c140ecbb8316d8d571ebd0ca8
gooniesDev <gooniesdev@gooniess-MacBook-Pro.local>
2021-10-16 19:34:24 -0300

    Agregando proyecto base

---
+++ Ionic-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
