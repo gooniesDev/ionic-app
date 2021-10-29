232000f70b3810a6c755c5a31b8637b16a8886c2
gooniesDev <gooniesdev@gooniess-MacBook-Pro.local>
2021-10-29 17:54:44 -0300

    Entrega 1

---
+++ Ionic-app/android/app/MainActivity.java
@@ -0,0 +1,4 @@
+import java.util.ArrayList;
+import com.getcapacitor.community.fcm.FCMPlugin;
+
+add(FCMPlugin.class);
--- Ionic-app/src/App.vue
+++ Ionic-app/src/App.vue
@@ -4,7 +4,7 @@
   </ion-app>
 </template>
 
-<script lang="ts">
+<script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
@@ -15,4 +15,39 @@ export default defineComponent({
     IonRouterOutlet
   }
 });
+/*
+data(){
+  return {
+    fcm: new FCM();
+  }
+}
+
+PushNotifications.requestPermission().then(result => {
+      alert("result " + JSON.stringify(result));
+    });
+PushNotifications.addListener("registrationError", (error) => {
+          console.log(`error on register ${JSON.stringify(error)}`);
+        }),
+        // Add tification received
+          PushNotifications.addListener(
+            "pushNotificationReceived",
+            (notification) => {
+              console.log(`notification ${JSON.stringify(notification)}`);
+            }
+          ),
+          // Add Action performed
+          PushNotifications.addListener(
+            "pushNotificationActionPerformed",
+            async (notification) => {
+                alert("notification " + notification)
+              console.log("notification succeeded");
+            }
+          ),
+          // Initialize the registration with FCM Token
+          PushNotifications.register();
+        const fcmToken = this.fcm.getToken();
+       alert(JSON.stringify(fcmToken));
+        console.log("token:" + JSON.stringify(fcmToken));
+  },
+*/
 </script>
